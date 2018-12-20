0b9ddd9ffd97b615ed0af54df860575c8e4afca1
iVapor <yangyy16@outlook.com>
2018-12-21 07:40:27 +0800

    change default config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
     enemy_speed: 5,
     bullet_speed: 5,
     fire_cooldown: 9,
-    pipe_space: 200,
-    pipe_narrow: 300,
+    pipe_space: 150,
+    pipe_narrow: 200,
     bird_speed: 2,
 }
